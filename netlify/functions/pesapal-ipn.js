226bd789fa298b34e86ab2e4b98303ad094e6bc7
Tonie 404 <info.onchari@gmail.com>
2026-06-12 12:52:18 +0000

    intial commit

--- netlify/functions/pesapal-ipn.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    console.log("IPN:", event.queryStringParameters);
-    return {
-        statusCode: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ status: 'OK' })
-    };
-};
